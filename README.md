0b9253d58d0b066601122a780e22bb7b17217278
Amirsina A. Albekord <aalbekor@uci.edu>
2025-10-22 16:11:44 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Week 3 Lab: On My Grind
+
+## CodePath WEB 102 Group 6
+
+Group Members: Jovanna Montemayor, Sudarshan Tiwari, Vishakha Mishra, Amirsina Albekord, Jonathan Hill, Damien Dubowitz
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    const API_KEY = import.meta.env.VITE_APP_API_KEY
+    const [list, setList] = useState(null)
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default App
