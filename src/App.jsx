be146e81edbcba16b256aab2502717a33631cf27
Amirsina A. Albekord <aalbekor@uci.edu>
2025-10-22 17:08:41 -0700

    searchbar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import CoinInfo from "./components/CoinInfo"
 function App() {
     const API_KEY = import.meta.env.VITE_APP_API_KEY
     const [list, setList] = useState(null)
+    const [filteredResults, setFilteredResults] = useState([])
+    const [searchInput, setSearchInput] = useState("")
 
     useEffect(() => {
         const fetchAllCoinData = async () => {
@@ -14,21 +16,68 @@ function App() {
         }
         fetchAllCoinData().catch(console.error)
     }, [])
+
+    const searchItems = searchValue => {
+        setSearchInput(searchValue)
+        if (searchValue !== "") {
+            const filteredData = list.Data.filter((item) =>
+                Object.values(item.CoinInfo)
+                    .join("")
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase())
+            )
+            setFilteredResults(filteredData)
+        } else {
+            setFilteredResults(list.Data)
+        }
+    }
+
     console.log(list)
     return (
         <>
             <div className="whole-page">
                 <h1>My Crypto List</h1>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(e) => searchItems(e.target.value)}
+                />
                 <ul>
-                    {list?.Data
-                        .map(data => data.CoinInfo).map(coinData => (
-                            <CoinInfo
-                                image={coinData.ImageUrl}
-                                name={coinData.FullName}
-                                symbol={coinData.Name}
-                            />
+                    {searchInput.length > 0
+                        ? filteredResults
+                            .map(coin => {
+                                const coinData = coin.CoinInfo
+                                if (
+                                    coinData.Algorithm !== "N/A" &&
+                                    coinData.ProofType !== "N/A"
+                                ) {
+                                    return (
+                                        <CoinInfo
+                                            key={coinData.Name}
+                                            image={coinData.ImageUrl}
+                                            name={coinData.FullName}
+                                            symbol={coinData.Name}
+                                        />
+                                    )
+                                }
+                                return null
+                            })
+                        : list?.Data // if no searchInput
+                            .map(data => data.CoinInfo)
+                            .filter(
+                                (coinData) =>
+                                    coinData.Algorithm !== "N/A" &&
+                                    coinData.ProofType !== "N/A"
+                            )
+                            .map(coinData => (
+                                <CoinInfo
+                                    key={coinData.Name}
+                                    image={coinData.ImageUrl}
+                                    name={coinData.FullName}
+                                    symbol={coinData.Name}
+                                />
+                            ))}
 
-                        ))}
                 </ul>
             </div>
         </>
