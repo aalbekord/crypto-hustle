1058bb0f6a11384a15cb58eef9ba4c3c130c3a8b
Amirsina A. Albekord <aalbekor@uci.edu>
2025-10-28 22:13:49 -0700

    lab start code

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// added imports
+import { BrowserRouter, Route, Routes } from "react-router"
+import Layout from './routes/Layout'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<App />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
 )
